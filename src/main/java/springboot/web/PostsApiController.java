abdb7f809a189dacea364585216bb603146da914
chungmin23 <dlcndals2005@naver.com>
2022-05-23 17:18:55 +0900

    jpa 추가

---
+++ src/main/java/springboot/web/PostsApiController.java
@@ -0,0 +1,31 @@
+package springboot.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import springboot.domain.posts.PostsService;
+import springboot.web.dto.PostsResponseDto;
+import springboot.web.dto.PostsSaveRequestDto;
+import springboot.web.dto.PostsUpdateRequestDto;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto reqeustDto){
+        return postsService.update(id,reqeustDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id)
+    {
+        return postsService.findById(id);
+    }
+}
