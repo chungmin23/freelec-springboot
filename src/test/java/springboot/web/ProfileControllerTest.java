5bdddb5120e40b9e79111d5e3ea88f3ff1dc829a
이충민 <dlcndals2005@naver.com>
2022-06-03 14:56:01 +0900

    real 프로필 추가

--- src/test/java/springboot/web/ProfileControllerTest.java
+++ src/test/java/springboot/web/ProfileControllerTest.java
@@ -17,6 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProfileControllerTest {
 
+//
     @LocalServerPort
     private int port;
 
